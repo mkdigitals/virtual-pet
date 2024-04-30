af802ba665eac8941912284c07fb5c3558802006
mkdigitals <mustafa.karadayi@yahoo.com>
2024-04-30 15:47:27 +0300

    walk and feed functions are defined

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -6,11 +6,50 @@ describe('constructor', () => {
   });
 });
 
-describe('ageAndGrowing', () => {
-  it('returns an object', () => {
+describe('birthAndGrowth', () => {
+  it('tests the effects of the growUp function on age, hunger and fitness of the object', () => {
     testObject = new Pet('Fido');
     expect(testObject.age).toEqual(0);
+    expect(testObject.hunger).toEqual(0);
+    expect(testObject.fitness).toEqual(10);
     testObject.growUp();
     expect(testObject.age).toEqual(1);
+    expect(testObject.hunger).toEqual(5);
+    expect(testObject.fitness).toEqual(7);
+  });
+});
+
+describe('walkAndGetFit', () => {
+  it('tests the effect of the walk function on fitness of the object', () => {
+    testObject = new Pet('Fido');
+    expect(testObject.fitness).toEqual(10);
+    testObject.growUp();
+    expect(testObject.fitness).toEqual(7);
+    testObject.growUp();
+    expect(testObject.fitness).toEqual(4);
+    testObject.walk();
+    expect(testObject.fitness).toEqual(8);
+    testObject.walk();
+    expect(testObject.fitness).toEqual(10);
+  });
+});
+
+
+describe('eatAndGetFed', () => {
+  it('tests the effect of the feed function on hunger of the object', () => {
+    testObject = new Pet('Fido');
+    expect(testObject.hunger).toEqual(0);
+    testObject.growUp();
+    expect(testObject.hunger).toEqual(5);
+    testObject.growUp();
+    expect(testObject.hunger).toEqual(10);
+    testObject.feed();
+    expect(testObject.hunger).toEqual(7);
+    testObject.feed();
+    expect(testObject.hunger).toEqual(4);
+    testObject.feed();
+    expect(testObject.hunger).toEqual(1);
+    testObject.feed();
+    expect(testObject.hunger).toEqual(0);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,12 +1,37 @@
+const MAX_FITNESS = 10;
+const WALK_FITNESS = 4;
+const MIN_HUNGER = 0;
+const GROWUP_HUNGER = 5;
+const GROWUP_AGE = 1;
+const GROWUP_FITNESS = -3;
+const FEED_HUNGER = -3;
 function Pet(name) {
    this.name = name;
    this.age = 0;
+   this.hunger = 0;
+   this.fitness = 10;
 }
 
 Pet.prototype = {
   growUp () {
-    this.age += 1; 
-  }
+    this.age += GROWUP_AGE;
+    this.hunger += GROWUP_HUNGER;
+    this.fitness += GROWUP_FITNESS;
+  },
+
+  walk () {
+    this.fitness += WALK_FITNESS;
+    if (this.fitness > MAX_FITNESS) {
+      this.fitness = MAX_FITNESS;
+    }
+  },
+
+  feed () {
+    this.hunger += FEED_HUNGER;
+    if (this.hunger < MIN_HUNGER) {
+      this.hunger = MIN_HUNGER;
+    }
+  },
 }
 
 module.exports = Pet;
